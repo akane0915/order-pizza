37e27b2e1ae8eeddc9c86b2c6adb879170da732b
asia kane <asialkane@gmail.com>
2017-04-07 09:38:24 -0700

    add JS logic to collect user input and store as variables in UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-//Business Logic
+//BUSINESS LOGIC
+//Objects
+function Pizza(size, toppings){
+  this.size = size;
+  this.toppings = toppings;
+}
 
 
 
-//User Interface Logic
+//Prototypes
+
+
+//UI LOGIC
 $(document).ready(function(){
+  $("form.order-form").submit(function(event){
+    event.preventDefault();
+    var nameInput = $("#customer-name-input").val();
+    var sizeInput = $("#pizza-size-input").val();
+    var toppingsInput = [];
+    $("#pizza-toppings input:checkbox[name=topping]:checked").each(function(){
+      toppingsInput.push($(this).val());
+    });
 
+    $(".output").text(nameInput + sizeInput + toppingsInput);
+    console.log(toppingsInput);
+
+  }); //Order form submit close
 }); //Doc ready close
