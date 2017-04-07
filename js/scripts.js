dd56a92916f9f27717e3c4bd66dfb884745fcded
asia kane <asialkane@gmail.com>
2017-04-07 09:57:57 -0700

    add js logic to create pizza and customer objects and pass user input into object properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //BUSINESS LOGIC
 //Objects
+function Customer(name){
+  this.name = name;
+  this.order = []; //Array of pizza(s)
+}
+
 function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
 }
-
-
 
 //Prototypes
 
@@ -14,6 +17,7 @@ function Pizza(size, toppings){
 $(document).ready(function(){
   $("form.order-form").submit(function(event){
     event.preventDefault();
+    //Collect User Input
     var nameInput = $("#customer-name-input").val();
     var sizeInput = $("#pizza-size-input").val();
     var toppingsInput = [];
@@ -21,8 +25,14 @@ $(document).ready(function(){
       toppingsInput.push($(this).val());
     });
 
+    //Create object variables
+    var pizzaOne = new Pizza(sizeInput, toppingsInput);
+    var customerOne = new Customer(nameInput);
+    customerOne.order.push(pizzaOne);
+    console.log(customerOne);
+
+    //Display output
     $(".output").text(nameInput + sizeInput + toppingsInput);
-    console.log(toppingsInput);
 
   }); //Order form submit close
 }); //Doc ready close
