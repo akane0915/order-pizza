68499a741f251c174bc49b6f8c18a58ff5e02dc4
asia kane <asialkane@gmail.com>
2017-04-07 10:08:31 -0700

    add prototype to calculate cost of pizza based on size

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,25 @@
 function Customer(name){
   this.name = name;
   this.order = []; //Array of pizza(s)
+  this.orderCost; //Sum of pizza cost(s)
 }
 
 function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
+  this.cost;
 }
 
 //Prototypes
-
+Pizza.prototype.singlePieCost = function () {
+  if (this.size === "small"){
+    this.cost = 12;
+  } else if (this.size === "medium"){
+    this.cost = 15;
+  } else {
+    this.cost = 18;
+  }
+}
 
 //UI LOGIC
 $(document).ready(function(){
@@ -29,10 +39,13 @@ $(document).ready(function(){
     var pizzaOne = new Pizza(sizeInput, toppingsInput);
     var customerOne = new Customer(nameInput);
     customerOne.order.push(pizzaOne);
-    console.log(customerOne);
+
+    //Call prototype to calculate cost of pizzaOne
+    pizzaOne.singlePieCost();
+
 
     //Display output
-    $(".output").text(nameInput + sizeInput + toppingsInput);
+    $(".output").text("Cost = $" + pizzaOne.cost);
 
   }); //Order form submit close
 }); //Doc ready close
